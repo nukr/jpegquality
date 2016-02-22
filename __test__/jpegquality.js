53d0dc87ded14e94e3d4c15345ab9d21221c8231
Nukr <nukrs.w@gmail.com>
2016-02-22 13:47:08 +0800

    add test case and fix bugs

--- __test__/jpegquality.js
+++ __test__/jpegquality.js
@@ -10,7 +10,7 @@ describe('jpegquality', function () {
       if (err) return console.error(err)
       var buf = new Buffer(data)
       var result = jpegquality(buf)
-      expect(parseInt(result, 10)).to.be.equal(1)
+      expect(Math.round(result)).to.be.equal(1)
       done()
     })
   })
@@ -20,7 +20,7 @@ describe('jpegquality', function () {
       if (err) return console.error(err)
       var buf = new Buffer(data)
       var result = jpegquality(buf)
-      expect(parseInt(result, 10)).to.be.equal(70)
+      expect(Math.round(result, 10)).to.be.equal(70)
       done()
     })
   })
@@ -30,7 +30,37 @@ describe('jpegquality', function () {
       if (err) return console.error(err)
       var buf = new Buffer(data)
       var result = jpegquality(buf)
-      expect(parseInt(result, 10)).to.be.equal(92)
+      expect(Math.round(result, 10)).to.be.equal(92)
+      done()
+    })
+  })
+
+  it('200%', function (done) {
+    fs.readFile(path.join(__dirname, './200%.jpg'), function (err, data) {
+      if (err) return console.error(err)
+      var buf = new Buffer(data)
+      var result = jpegquality(buf)
+      expect(Math.round(result)).to.be.equal(98)
+      done()
+    })
+  })
+
+  it('4 table 99%', function (done) {
+    fs.readFile(path.join(__dirname, './4-table-99%.jpg'), function (err, data) {
+      if (err) return console.error(err)
+      var buf = new Buffer(data)
+      var result = jpegquality(buf)
+      expect(Math.round(result)).to.be.equal(99)
+      done()
+    })
+  })
+
+  it('one table 99%', function (done) {
+    fs.readFile(path.join(__dirname, './one-table-99.jpg'), function (err, data) {
+      if (err) return console.error(err)
+      var buf = new Buffer(data)
+      var result = jpegquality(buf)
+      expect(Math.round(result)).to.be.equal(99)
       done()
     })
   })
